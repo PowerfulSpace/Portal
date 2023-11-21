cdfc77a75271ee47d3ab3484afc1cbb12d0892be
Alexander <sany199574@gmail.com>
2023-11-21 22:15:29 +0300

    Добавлена базовая часть для правильной работы конфигураций сервисов

---
+++ src/PS.Portal/PS.Portal.Web/Definitions/Base/AppDefinitions.cs
@@ -0,0 +1,15 @@
+﻿using PS.Portal.Web.Definitions.Base.Interfaces;
+
+namespace PS.Portal.Web.Definitions.Base
+{
+    public abstract class AppDefinitions : IAppDefinitions
+    {
+        public virtual void ConfigureApplication(WebApplication app, IWebHostEnvironment environment)
+        {
+        }
+
+        public virtual void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+        }
+    }
+}
