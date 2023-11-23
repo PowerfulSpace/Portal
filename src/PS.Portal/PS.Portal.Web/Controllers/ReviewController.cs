ea74854dc4ba1cfa508ad7feb77170171779d116
Alexander <sany199574@gmail.com>
2023-11-24 00:00:43 +0300

    Добавлены контроллеры. Пустые  - ActorController - CountryController - GenreController - ProducerController - ReviewController

---
+++ src/PS.Portal/PS.Portal.Web/Controllers/ReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.Portal.Web.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
