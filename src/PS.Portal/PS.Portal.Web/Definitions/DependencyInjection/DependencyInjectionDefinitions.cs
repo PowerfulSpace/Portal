5235c53985b3a886f70518855ecbc02d3e6bba6b
Alexander <sany199574@gmail.com>
2023-11-25 00:10:35 +0300

    Добавлены сервисы в механизм внедрений коллекий сервсов
    ActorRepository
    CountryRepository
    GenreRepository
    ProducerRepository
    ReviewRepository

--- src/PS.Portal/PS.Portal.Web/Definitions/DependencyInjection/DependencyInjectionDefinitions.cs
+++ src/PS.Portal/PS.Portal.Web/Definitions/DependencyInjection/DependencyInjectionDefinitions.cs
@@ -9,6 +9,11 @@ namespace PS.Portal.Web.Definitions.DependencyInjection
         public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IMovie, MovieRepository>();
+            services.AddScoped<IActor, ActorRepository>();
+            services.AddScoped<ICountry, CountryRepository>();
+            services.AddScoped<IGenre, GenreRepository>();
+            services.AddScoped<IProducer, ProducerRepository>();
+            services.AddScoped<IReview, ReviewRepository>();
         }
     }
 }
