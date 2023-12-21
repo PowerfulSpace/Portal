1b96edfc6e7476faeb978da62ee14180195b1417
Alexander <sany199574@gmail.com>
2023-12-21 12:39:07 +0300

    Добавлен контроллер для отображения только минимальной информации о фильмах

---
+++ src/PS.Portal/PS.Portal.Web/Controllers/AllMovieController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PS.Portal.DAL.Interfaces;
+using PS.Portal.Domain.Entities;
+using PS.Portal.Domain.Models;
+
+namespace PS.Portal.Web.Controllers
+{
+    public class AllMovieController : Controller
+    {
+        private readonly IMovie _movieRepository;
+
+        public AllMovieController(IMovie movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public async Task<IActionResult> Index(string sortExpression = "", string searchText = "", int currentPage = 1, int pageSize = 5)
+        {
+            SortModel sortModel = new SortModel();
+            sortModel.AddColumn("name");
+            sortModel.AddColumn("description");
+            sortModel.AddColumn("rating");
+            sortModel.AddColumn("yearShown");
+            sortModel.AddColumn("filmDuration");
+            sortModel.AddColumn("acceptableAge");
+            sortModel.AddColumn("isReaded");
+            sortModel.AddColumn("partFilm");
+            sortModel.AddColumn("country");
+            sortModel.AddColumn("producer");
+            sortModel.ApplySort(sortExpression);
+
+            PaginatedList<Movie> movies = await _movieRepository.GetItemsAsync(sortModel.SortedProperty, sortModel.SortedOrder, searchText, currentPage, pageSize);
+
+            var pager = new PagerModel(movies.TotalRecords, currentPage, pageSize);
+            pager.SortExpression = sortExpression;
+            pager.SearchText = searchText;
+
+            ViewBag.Pager = pager;
+
+            ViewData["SortModel"] = sortModel;
+            ViewBag.SearchText = searchText;
+
+            TempData["SearchText"] = searchText;
+            TempData.Keep("SearchText");
+
+            TempData["PageSize"] = pageSize;
+            TempData.Keep("PageSize");
+
+            TempData["CurrentPage"] = currentPage;
+            TempData.Keep("CurrentPage");
+
+            return View(movies);
+        }
+    }
+}
